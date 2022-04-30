68642b7ef660749dde46d68a96d7c93cfecce954
Andres Martinez <andre.mtzd07@gmail.com>
2022-04-29 22:43:06 -0500

    API request as an authenticated user

--- src/Service.ts
+++ src/Service.ts
@@ -1,9 +1,5 @@
 import ServerError from "./ServerError";
-
-const headers = {
-    "Accept": "application/json",
-    "Content-Type": "application/json"
-}
+import { Buffer } from "buffer";
 
 enum HTTP {
     GET = "GET",
@@ -13,17 +9,29 @@ enum HTTP {
 }
 
 class Service {
+    headers: any;
     domain: string;
     options: any;
 
-    constructor() {
+    constructor(username: string, password: string) {
+        const basic = Buffer.from((username + ":" + password)).toString('base64');
         this.domain = "http://localhost:5000/";
+        this.headers = {
+            "Accept": "application/json",
+            "Content-Type": "application/json",
+            "Authorization": "Basic " + basic,
+        };
     }
 
-    async request<TResponse>(requestURL:string, method:HTTP): Promise<TResponse> {
+    async request<TResponse>(requestURL:string, method:HTTP, body:any = undefined): Promise<TResponse> {
         let url:string = new URL(requestURL, this.domain).href;
         console.log(url);
-        this.options = {headers, method};
+        if (body === undefined)
+            body = "";
+        else
+            body = JSON.stringify(body);
+        console.log(body);
+        this.options = {headers: this.headers, method, body};
         
         let response = await fetch(url, this.options);
         return response.json()
@@ -36,6 +44,10 @@ class Service {
 
     get<TResponse>(requestURL:string): Promise<TResponse> {
         return this.request<TResponse>(requestURL, HTTP.GET);
+    }
+
+    post<TResponse>(requestURL: string, body: any): Promise<TResponse> {
+        return this.request(requestURL, HTTP.POST, body);
     }
 }
 
