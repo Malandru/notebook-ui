660f4eb38b1797d89a9f7117d5745d2a930c639f
Andres Martinez <andre.mtzd07@gmail.com>
2022-05-02 20:12:28 -0500

    Using routes to display different components according to the path

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,22 @@
-import React, { useState } from 'react';
 import logo from './logo.svg';
 import './css/App.css';
-import User from './user/User'
-import ServerError from './api/ServerError';
-import { Button, Card, TextField } from '@mui/material';
-import API from './api/Rest';
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Login from 'xdomain/Login';
 
 function App() {
-  const user:User = new User();
+  return (
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Login />} />
+      <Route path='/app' element={<ExampleApp />} />
+    </Routes>
+    </BrowserRouter>
+  );
+}
 
-  const handleUsernameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    user.username = event.target.value;
-  }
-
-  const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    user.password = event.target.value;
-  }
-
-  function handleLogin(){
-    const response = API.login(user);
-    response.then(user => console.log(user))
-      .catch(serverError => console.log(serverError));
-  }
-
+function ExampleApp() {
   return (
     <div className="App">
-      <Card>
-        <p>A ver</p>
-      </Card>
-      <TextField
-        required
-        id="outlined-required-username"
-        label="Username"
-        onChange={handleUsernameChange}
-      />
-      <TextField
-        required
-        id="outlined-user-password"
-        label="Password"
-        type="password"
-        autoComplete="current-password"
-        onChange={handlePasswordChange}
-      />
-      <Button variant="contained" onClick={handleLogin}>Login</Button>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -58,7 +32,7 @@ function App() {
         </a>
       </header>
     </div>
-  );
+  )
 }
 
 export default App;
