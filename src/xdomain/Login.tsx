dbe70b116cbbbd71f480631d1cef96f6b602c98e
Andres Martinez <andre.mtzd07@gmail.com>
2022-05-03 15:15:06 -0500

    Better login UI

--- src/xdomain/Login.tsx
+++ src/xdomain/Login.tsx
@@ -1,42 +1,79 @@
 import React from 'react';
-import { Button, TextField } from '@mui/material';
+import { Avatar, Box, Button, Checkbox, Container, createTheme, CssBaseline, FormControlLabel, Grid, Link, TextField, ThemeProvider, Typography } from '@mui/material';
 import API from 'api/Rest';
 import User from 'user/User'
 import { useNavigate } from 'react-router-dom';
+import { LockOutlined } from '@mui/icons-material';
 
 function Login() {
-    const user:User = new User();
+    const user: User = new User();
     const navigate = useNavigate();
 
     function handleLogin(event: React.SyntheticEvent) {
         event.preventDefault();
         const response = API.login(user);
         response.then(user => {
-            navigate('/', {state: user})
+            navigate('/', { state: user })
         })
             .catch(serverError => console.log(serverError));
     }
 
     return (
-        <div>
-            <form onSubmit={handleLogin}>
-                <TextField
-                required
-                id='outlined-username'
-                label='Username'
-                onChange={e => user.username = e.target.value}
-                />
-                <TextField
-                required
-                id='outlined-password'
-                label='Password'
-                type="password"
-                autoComplete="current-password"
-                onChange={e => user.password = e.target.value}
-                />
-                <Button variant="contained" type="submit">Login</Button>
-            </form>
-        </div>
+        <ThemeProvider theme={createTheme()}>
+            <Container component="main" maxWidth="xs">
+                <Box className='AvatarBox'
+                sx={{
+                    marginTop: 8,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                }}
+                >
+                    <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                        <LockOutlined />
+                    </Avatar>
+                    <Typography component="h1" variant="h5">Sign in</Typography>
+                    <Box component="form" onSubmit={handleLogin} noValidate sx={{ mt: 1 }}>
+                        <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="username"
+                            label="Username"
+                            name="username"
+                            autoFocus
+                            onChange={e => user.username = e.target.value}
+                        />
+                        <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            name="password"
+                            label="Password"
+                            type="password"
+                            id="password"
+                            autoComplete="current-password"
+                            onChange={e => user.password = e.target.value}
+                        />
+                        <FormControlLabel
+                            control={<Checkbox value="remember" color="primary" />}
+                            label="Remember me"
+                        />
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            sx={{ mt: 3, mb: 2 }}
+                        >Sign In</Button>
+
+                        <Link href="#" variant="body2">
+                            {"Don't have an account? Sign Up"}
+                        </Link>
+                    </Box>
+                </Box>
+            </Container>
+        </ThemeProvider>
+
     );
 }
 
